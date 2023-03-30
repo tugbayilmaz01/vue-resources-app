628c78e126664d56695966bc1c6a73312fc5bf74
tugbayilmaz01 <97852394+tugbayilmaz01@users.noreply.github.com>
2023-03-30 10:11:30 +0300

    added the header & baseCard components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
+  <the-header title="RememberMe"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
 export default {
   components: {
     StoredResources,
+    TheHeader,
   },
   data() {
     return {
@@ -28,6 +32,7 @@ export default {
   },
 };
 </script>
+
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -1,22 +1,24 @@
 <template>
   <li>
-    <div>
+    <base-card>
       <header>
         <h3>{{ title }}</h3>
         <button>Delete</button>
       </header>
-    </div>
-    <p>{{ description }}</p>
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
+
 <script>
 export default {
   props: ['title', 'description', 'link'],
 };
 </script>
+
 <style scoped>
 li {
   margin: auto;
